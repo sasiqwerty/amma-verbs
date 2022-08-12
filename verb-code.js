f69fe96b698883eecf3ad103266588c6d4964fd0
Sarath Sasi <sarathsasi.g@gmail.com>
2022-08-12 18:19:40 +0530

    added color option

--- verb-code.js
+++ verb-code.js
@@ -55,4 +55,7 @@ const verbs = [
 function verbDisplay() {
   document.getElementById("verb-dis").innerHTML =
     verbs[Math.floor(Math.random() * verbs.length)];
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  color.innerHTML = "#" + randomColor;
 }
